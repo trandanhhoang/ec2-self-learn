528161e4ff0f9735b203b29c12a5098c33d7a28b
hoangtran <hoang.tran@galaxyfinx.com>
2024-03-11 13:51:49 +0700

    push sit

--- src/main/java/com/example/ec2selflearn/HomeController.java
+++ src/main/java/com/example/ec2selflearn/HomeController.java
@@ -1,16 +1,19 @@
 
 package com.example.ec2selflearn;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
+@Slf4j
 public class HomeController {
 
     @GetMapping
     public String test() {
+        log.info("hehe");
         return "Tran Danh Hoang trigger" + "service1";
     }
 
